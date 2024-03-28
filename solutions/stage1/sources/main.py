475aced702207e466816312fae1938cea6d72137
Abdoulaye Balde <abdoulaye.balde@epita.fr>
2024-03-28 17:00:32 +0100

    hello world for stage1

--- solutions/stage1/sources/main.py
+++ solutions/stage1/sources/main.py
@@ -1,15 +1,7 @@
-import numpy as np
 import gradio as gr
 
-def sepia(input_img):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189],
-        [0.349, 0.686, 0.168],
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = input_img.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return sepia_img
+def greet(name):
+    return "Hello " + name + "!"
 
-demo = gr.Interface(sepia, gr.Image(), "image")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch(server_name="0.0.0.0")
